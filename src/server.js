fcb68111d8162a7ec94a2a94bc0c018cd6a301b5
Van Dao <dao28901@gmail.com>
2024-12-19 09:43:52 +0700

    connectionPool

--- src/server.js
+++ src/server.js
@@ -5,17 +5,11 @@ require("dotenv").config();
 const port = process.env.PORT || 8081;
 const hostname = process.env.HOST_NAME;
 const configViewEngine = require("./config/viewEngine");
-const mysql = require("mysql2");
 const webRoutes = require("./routers/web");
+const connection = require("./config/database");
 configViewEngine(app);
 app.use("/", webRoutes);
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "123456",
-  database: "hoidanit",
-});
+
 connection.query("SELECT * FROM `Users` ", (err, result) => {
   console.log(">>>", result);
 });
